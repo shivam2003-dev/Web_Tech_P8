a0e524546fdb3fa49a3418bb1291adeed3463a8a
Shivam Kumar <78433942+shivam2003-dev@users.noreply.github.com>
2022-06-01 21:49:43 +0530

    Add files via upload

---
+++ Content Management System/index.php
@@ -0,0 +1,88 @@
+<?php
+$insert = false;
+if (isset($_POST['name'])) {
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if (!$con) {
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+
+    // Collect post variables
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $desc = $_POST['desc'];
+
+    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+
+    // Execute the query
+    if ($con->query($sql) == true) {
+        // Flag for successful insertion
+        $insert = true;
+    } else {
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <img class="bg" src="travel1.jpeg" alt="">
+    <div class="container">
+        <h1>Welcome to World Travel & Tour Trip form</h3>
+            <p>Enter your details and submit this form to confirm your participation in the trip </p>
+            <?php
+            if ($insert == true) {
+                echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the trip</p>";
+            }
+            ?>
+            <form action="index.php" method="post">
+                <input type="text" name="name" id="name" placeholder="Enter your name">
+                <input type="text" name="age" id="age" placeholder="Enter your Age">
+                <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+                <input type="email" name="email" id="email" placeholder="Enter your email">
+                <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+                <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+                <button onclick="myFunction()" class="btn">Submit</button>
+            </form>
+            <br />
+            <br />
+
+            <div style="text-align:center">
+                <a href="Login_Page.html">Goto Login Page
+
+                </a>
+            </div>
+    </div>
+
+    <script>
+        function myFunction() {
+            location.replace("index.html")
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ Content Management System/trip.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2022 at 12:23 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `trip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trip`
+--
+
+CREATE TABLE `trip` (
+  `sno` int(100) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `age` int(3) NOT NULL,
+  `gender` varchar(8) NOT NULL,
+  `email` varchar(22) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `other` varchar(255) NOT NULL,
+  `dt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trip`
+--
+
+INSERT INTO `trip` (`sno`, `name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES
+(1, 'Deepak Mahto', 20, 'Male', 'deepak12@gmail.com', 1245786325, 'Good trip', '2022-05-30 12:50:52');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
